bb9049c2cdcea0ae3e17377dd0ee3549a79b9973
Muhtasim-Fuad-Showmik <49075537+Muhtasim-Fuad-Showmik@users.noreply.github.com>
2021-07-27 03:31:21 +0600

    Revert "Change database configuration"
    This reverts commit 45c83867c19d2a9b15d2e84f7a81f255d4f565e9.

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'qcizUvgC16' );
+define( 'DB_NAME', 'brainsharp' );
 
 /** MySQL database username */
-define( 'DB_USER', 'qcizUvgC16' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'b9nrJK91bp' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'remotemysql.com' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
